0b086129e2fa4cba86ac410af0e7adf0de7a8544
Yusuke Saito <ysaito8015@gmail.com>
2022-01-03 17:45:54 +0100

    update ch03

--- ch03/pento/config/dev.exs
+++ ch03/pento/config/dev.exs
@@ -24,7 +24,7 @@ config :pento, Pento.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :pento, PentoWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 8000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
